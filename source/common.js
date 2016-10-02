9a13abc97513b4da8cda02f8b13b7e7c7b7201b8
vankai <625608852@qq.com>
2016-10-02 21:32:48 +0800

    add a little plugin

--- source/common.js
+++ source/common.js
@@ -5,11 +5,11 @@
 
 (function() {
     //make top menu active
-    var nav_toggle = document.getElementById("js-toggle-nav"),
-        arr_span = nav_toggle.firstElementChild.classList,
-        arr_ul = nav_toggle.nextElementSibling.classList;
+    var a = document.getElementById("js-toggle-nav"),
+        arr_span = a.firstElementChild.classList,
+        arr_ul = a.nextElementSibling.classList;
 
-    nav_toggle.addEventListener("click", function() {
+    a.addEventListener("click", function() {
 
         if (arr_span.contains('active')) {
             arr_span.remove('active');
@@ -19,5 +19,184 @@
             arr_ul.add('active');
         }
     });
+
+})();
+
+(function(){
+  //scroll top
+  function throttle(func, wait) {
+      var ctx, args, rtn, timeoutID; // caching
+      var last = 0;
+
+      return function throttled() {
+          ctx = this;
+          args = arguments;
+          var delta = new Date() - last;
+          if (!timeoutID)
+              if (delta >= wait) call();
+              else timeoutID = setTimeout(call, wait - delta);
+          return rtn;
+      };
+
+      function call() {
+          timeoutID = 0;
+          last = +new Date();
+          rtn = func.apply(ctx, args);
+          ctx = null;
+          args = null;
+      }
+
+
+  }
+
+  // add style tag
+  var style_tag = document.createElement("style"),
+      link_tag = document.createElement("a");
+  style_tag.innerText = ".HideS-Au2o-top { position: fixed; display: inline-block; right: -50px; bottom: 40px; box-sizing: border-box; width: 40px; height: 40px; background-color: #fff; border-radius: 5px; box-shadow: 0 2px 5px 0 rgba(0, 0, 0, .2), 0 2px 10px 0 rgba(0, 0, 0, .12); transition: .3s; animation-duration: .9s; animation-fill-mode: forwards; animation-timing-function: cubic-bezier(0.62, 0, 0.2, 1.32); }"+
+      ".HideS-Au2o-top:before, .HideS-Au2o-top:after,.HideS-Au2o-top:hover:before ,.HideS-Au2o-top:hover:after { content: ''; display: inline-block; position: absolute; width: 20px; height: 4px; top: 10px; left: 10px; transform-origin: center center; background-color: #989898; transform: rotateZ(45deg) translateX(8px); }"+
+      ".HideS-Au2o-top:after,.HideS-Au2o-top:hover:after { right: 10px; transform: rotateZ(-45deg) translateX(-8px); }"+
+      ".HideS-Au2o-top:hover{background-color:#fff;}"+
+      " @keyframes HideS-Au2o-in { 0% { transform: translateX( 0px) rotateZ(360deg); } 100% { transform: translateX(-90px) rotateZ(0); } } @keyframes HideS-Au2o-out{ 0% { transform: translateX( -90px) rotateZ(0); } 100% { transform: translateX(0px) rotateZ(360); } }";
+  document.head.appendChild(style_tag);
+
+  link_tag.className = "HideS-Au2o-top material-shadow-hover ";
+  link_tag.id = "js-HideS-Au2o";
+  link_tag.href = "#";
+  document.body.appendChild(link_tag);
+  link_tag = style_tag = null;
+
+
+    //<a href="#" class="HideS-Au2o-top" id="js-HideS-Au2o"></a>
+
+
+
+  var ST = {
+      el: document.getElementById("js-HideS-Au2o"),
+      top:window.scrollY,
+      height:window.innerHeight,
+      status: "hide",
+      status_final: "",
+      timer:null,
+      timer_sleep:null,
+      hide: {
+          up: function(top,h) {
+            //console.log(top,h);
+            if(top>h){
+                ST._show();
+            }
+          },
+          down: function(top,h) {}
+      },
+      busy: {
+          up: function(top,h) {},
+          down: function(top,h) {}
+      },
+      show: {
+          up: function(top,h) {
+            //console.log(top,h);
+            if(top<=h){
+              ST._hide();
+            }
+          },
+          down: function(top,h) {
+            //console.log(top,h);
+              ST._hide();
+          }
+      },
+      judge: function() {
+
+          var top = window.scrollY,
+              distance = this.top - top,
+              direction = distance > 0 ? "up" : "down";
+
+          this[this.status][direction](top,this.height);
+
+          top = distance = direction = null;
+      },
+      _hide: function() {
+          //console.log("hide");
+          this.el.style.animationName = "HideS-Au2o-out";
+          this.status = 'busy';
+          this._sleep_till("hide");
+      },
+      _show: function() {
+          //console.log("show");
+          this.el.style.animationName = "HideS-Au2o-in";
+          this.status = 'busy';
+          this._sleep_till("show");
+      },
+      _update_top:function(time){
+        clearTimeout(this.timer);
+           this.timer = setTimeout(function() {
+             //console.log("update top & height");
+               ST.top = window.scrollY;
+               ST.height = window.innerHeight;
+           }, time || 1);
+      },
+      _sleep_till:function(status){
+        clearTimeout(this.timer_sleep);
+        this.timer_sleep = setTimeout(function(){
+          ST.status = status;
+          ST._wakeup();
+        },900);
+      },
+      _wakeup:function(){
+        if(this.status == this.status_final){
+          //do nothing
+        }else{
+
+        }
+        this._update_top();
+      }
+  };
+
+  var thr_scroll = throttle(function(){
+
+    ST.judge();
+    ST._update_top(200);
+    //console.log(ST);
+  }, 150);
+  window.addEventListener('scroll', thr_scroll);
+
+
+})();
+
+(function(){
+  //update time span
+  var arr_spans = document.getElementsByClassName("time-readable"),
+      Zh_cn =  ["年","个月","天"],
+
+      today = new Date();
+
+  var getString = function(str){
+      var arr = str.match(/(\d{4})-(\d{2})-(\d{2})/),
+          year = today.getFullYear() - Number(arr[1]),
+          month = today.getMonth() - Number(arr[2]) + 1,//the first month in JavaScript is zero.
+          date = today.getDate() - Number(arr[3]);
+
+          if(year !== 0){
+            return "" + year + Zh_cn[0] + "前";
+          }else{
+            if(month !== 0){
+              return "" + month +Zh_cn[1] + "前";
+            }else{
+              if(date !== 0){
+                return "" + date + Zh_cn[2] + "前";
+              }else{
+                return "今天";
+              }
+            }
+          }
+
+
+
+  };
+
+      arr_spans = [].slice.call(arr_spans);
+      arr_spans.forEach(function(el,index){
+        var a = el.innerHTML;
+        el.innerHTML = getString(a);
+      });
+
 
 })();
