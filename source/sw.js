dfa8d324e1ee429f3508b2d3a7d1951ae2d0bdd4
vankai <625608852@qq.com>
2017-01-20 17:12:49 +0800

    finish about page

--- source/sw.js
+++ source/sw.js
@@ -14,13 +14,13 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-201701010';
+const PRECACHE = 'precache-20170102';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  '/', 
   '/index.html',
+  '/', 
   '/css/all.css',
   '/common.js',
   '/img/photography.png',
