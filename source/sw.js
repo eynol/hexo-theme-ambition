377e29e70ce55b193a04dd74ca69aa117e0f2485
vankai <625608852@qq.com>
2017-01-21 17:47:17 +0800

    change cache rules

--- source/sw.js
+++ source/sw.js
@@ -14,26 +14,25 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-20170103';
+const PRECACHE = 'precache-20170104';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-  '/index.html',
-  '/', 
   '/css/all.css',
   '/common.js',
   '/img/photography.png',
   '/img/photography_@2X.png',
   '/img/yellow-metal.jpg',
+  '/404.html'
 ];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    .then(cache => cache.addAll(PRECACHE_URLS))
+    .then(self.skipWaiting())
   );
 });
 
@@ -57,21 +56,38 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+    if (navigator.onLine) {
+      //Online strategy
+      event.respondWith(caches.open(PRECACHE)
+        .then(cache => cache.match(event.request))
+        .then(cachedResponse => {
+          if (cachedResponse) {
+            //Request precached resources, return the response directly.
+            return cachedResponse
+          } else {
+            //onLine resources first , always get a new one.
+            return caches.open(RUNTIME).then(cache => {
+              return fetch(event.request).then(response => {
+                // Put a copy of the response in the runtime cache.
+                return cache.put(event.request, response.clone()).then(() => {
+                  return response
+                })
+              })
+            })
+          }
+        }))
+    } else {
+      //Off-line , Cache first, 404 second
+      event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+          if (cachedResponse) {
+            return cachedResponse
+          } else {
+            return caches.open(PRECACHE)
+                         .then(cache => cache.match("404.html"))
+          }
+        }))
+    }
   }
 });
